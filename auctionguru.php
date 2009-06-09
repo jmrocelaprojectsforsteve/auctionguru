742137ccccf87e08aa16c89176f91b0da9ffbe78
John Mark Rocela <me@iamjamoy.com>
2009-06-08 18:10:55 -0700

    Initial Commit

---
+++ auctionguru.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Plugin Name: 	AuctionGuru
+ * Plugin URI:	http://www.gurucs.com/products/auctionguru
+ * Description:	Enables the user to have auctions on their blog dynamically.
+ * @version:	1.0.0
+ * Author: 		J. Rocela (me@iamjamoy.com)
+ * Author URI:	http://www.gurucs.com
+ *
+ * copyright (c) 2009  J. Rocela(me@iamjamoy.com)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+ 
+?>
